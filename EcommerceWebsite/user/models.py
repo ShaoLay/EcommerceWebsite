7b182dfb18f4d49f8ae4ea210457cc4fd9c98354
lay <javs_shao@163.com>
2020-01-17 12:40:46 +0800

    注册前后端实现_解决无法获取email属性

--- EcommerceWebsite/user/models.py
+++ EcommerceWebsite/user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    email = models.EmailField(null=True, blank=True)
+    email = models.EmailField()
 
 
     def __str__(self):
